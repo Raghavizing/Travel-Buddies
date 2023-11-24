2ac6657d37b84b5d7576bf2197cc71fecf69d8f9
Raghav Chaturvedi <raghavchaturvedi@Raghavs-MacBook-Air.local>
2023-11-24 14:51:51 +0530

    Chnages in path

--- src/Components/Hotel.js
+++ src/Components/Hotel.js
@@ -30,7 +30,7 @@ const Hotel = [
       "id": 4,
       "name": "Historic Inn & Spa",
       "location": "Charleston, South Carolina",
-      "image": "../Images/Hotels/hotel4.jpg",
+      "image": "./Hotels/hotel4.jpg",
       "price": "$200/night",
       "rating": 4.4,
       "description": "Experience Southern charm in our historic inn. Pamper yourself with spa treatments and explore the historic district."
@@ -39,7 +39,7 @@ const Hotel = [
       "id": 5,
       "name": "Tropical Paradise Resort",
       "location": "Maui, Hawaii",
-      "image": "../Images/Hotels/hotel5.jpg",
+      "image": "./Hotels/hotel5.jpg",
       "price": "$400/night",
       "rating": 4.9,
       "description": "Relax in paradise at our beachfront resort. Enjoy white sand beaches, lush gardens, and watersports."
@@ -48,7 +48,7 @@ const Hotel = [
       "id": 6,
       "name": "Urban Elegance Hotel",
       "location": "Chicago, Illinois",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel6.jpg",
       "price": "$180/night",
       "rating": 4.3,
       "description": "Experience sophistication in the Windy City. Our hotel offers modern amenities and easy access to the city's attractions."
@@ -57,7 +57,7 @@ const Hotel = [
       "id": 7,
       "name": "Rustic Lakeside Cabin",
       "location": "Lake Tahoe, California",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel7.jpg",
       "price": "$220/night",
       "rating": 4.6,
       "description": "Embrace nature in our cozy lakeside cabin. Enjoy fishing, hiking, and stunning lake views."
@@ -66,7 +66,7 @@ const Hotel = [
       "id": 8,
       "name": "Seaside Villa Retreat",
       "location": "Santorini, Greece",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel8.jpg",
       "price": "$450/night",
       "rating": 4.9,
       "description": "Indulge in luxury in our cliffside villa. Witness breathtaking sunsets and explore the beauty of Santorini."
@@ -75,7 +75,7 @@ const Hotel = [
       "id": 9,
       "name": "Historic French Quarter Inn",
       "location": "New Orleans, Louisiana",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel9.jpg",
       "price": "$220/night",
       "rating": 4.5,
       "description": "Immerse yourself in the culture of the French Quarter. Stay in a historic inn with a courtyard and jazz music."
@@ -84,7 +84,7 @@ const Hotel = [
       "id": 10,
       "name": "Alpine Chalet Resort",
       "location": "Zermatt, Switzerland",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel10.jpg",
       "price": "$350/night",
       "rating": 4.7,
       "description": "Ski in the Swiss Alps and relax in our alpine chalet. Enjoy stunning mountain views and cozy evenings by the fire."
@@ -93,7 +93,7 @@ const Hotel = [
       "id": 11,
       "name": "Desert Oasis Retreat",
       "location": "Sedona, Arizona",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel11.jpg",
       "price": "$280/night",
       "rating": 4.6,
       "description": "Discover the magic of the desert in our oasis retreat. Hike, meditate, and enjoy starry nights."
@@ -102,7 +102,7 @@ const Hotel = [
       "id": 12,
       "name": "Garden City Guesthouse",
       "location": "Kyoto, Japan",
-      "image": "../Images/Hotels/hotel1.jpg",
+      "image": "./Hotels/hotel12.jpg",
       "price": "$220/night",
       "rating": 4.4,
       "description": "Experience traditional Japanese hospitality in our serene guesthouse. Explore Kyoto's historic temples and gardens."
