0030e216569e228518370de38f814422e3b5cbc3
Raghav Chaturvedi <raghavchaturvedi@192.168.1.13>
2023-12-01 19:48:23 +0530

    Added Modal

--- src/Components/Listings/Listings.jsx
+++ src/Components/Listings/Listings.jsx
@@ -1,47 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import Hotel from "../Hotel"
 import AOS from 'aos';
 import "./Listings.css";
 import 'aos/dist/aos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar} from '@fortawesome/free-solid-svg-icons'
-import {faLocationDot} from '@fortawesome/free-solid-svg-icons'
-import {faClipboardList} from '@fortawesome/free-solid-svg-icons'
-function Card(props) {
-    return (
-        <div className="card shadow col-lg-3 col-md-5 col-10 mx-3 my-2 rounded-2 text-left p-0 listing-card" data-aos="fade-up">
-            <img src={props.image} className="card-img-top listing-card-img" alt="..." />
-            <div className="card-body listing-card-body">
-                <div className="listing-card-header">
-                <h5 className="card-title listing-card-name">{props.name}</h5>
-                <p className="card-text"><FontAwesomeIcon icon={faLocationDot} style={{color: "#ffd700",}} /> {props.location}</p>
-                </div>
-                <div className="listing-card-details py-3 my-2 d-flex justify-content-between">
-                <p className="card-text"><FontAwesomeIcon icon={faStar} style={{color: "#ffd700",}} /> {props.rating}</p> 
-                <p className="card-text listing-card-price">{props.price}</p>
-                </div>
-                <p className="card-text">{props.description}</p>
-            </div>
-            <button className="btn listing-btn text-white px-3 rounded-4 mb-4 ms-2 col-4">Details  <FontAwesomeIcon icon={faClipboardList} style={{color: "#ffffff",}} /></button>
-        </div>
-    )
-}
-function createCard(props) {
-    return (
-        <Card key={props.id} image={props.image} name={props.name} location = {props.location} price={props.price} rating={props.rating} description={props.description} />
-    )
-}
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons'
+import HotelModal from "./hotelModal/hotelModal";
 const Listing = () => {
+    const [openModal, setOpenModal] = useState(false);
     useEffect(() => {
         AOS.init();
     }, [])
-    return (<div className="mt-5">
-        <div className="listingTitle col-11 ms-auto">Most Visited <span className="listingTitleUnderline">Destinations</span></div>
-        <div className="row d-flex justify-content-center">
+    function createCard(props) {
+        return (
+            <Card key={props.id} image={props.image} name={props.name} location={props.location} price={props.price} rating={props.rating} description={props.description} />
+        )
+    }
+    function Card(props) {
+        return (
+            <div className="card shadow col-lg-3 col-md-5 col-10 mx-3 my-2 rounded-2 text-left p-0 listing-card" data-aos="fade-up">
+                <img src={props.image} className="card-img-top listing-card-img" alt="..." />
+                <div className="card-body listing-card-body">
+                    <div className="listing-card-header">
+                        <h5 className="card-title listing-card-name">{props.name}</h5>
+                        <p className="card-text"><FontAwesomeIcon icon={faLocationDot} style={{ color: "#ffd700", }} /> {props.location}</p>
+                    </div>
+                    <div className="listing-card-details py-3 my-2 d-flex justify-content-between">
+                        <p className="card-text"><FontAwesomeIcon icon={faStar} style={{ color: "#ffd700", }} /> {props.rating}</p>
+                        <p className="card-text listing-card-price">{props.price}</p>
+                    </div>
+                    <p className="card-text">{props.description}</p>
+                </div>
+                <button className="btn listing-btn text-white px-3 rounded-4 mb-4 ms-2 col-4" onClick={() => { setOpenModal(true) }}>Details  <FontAwesomeIcon icon={faClipboardList} style={{ color: "#ffffff", }} /></button>
+            </div>
+        )
+    }
+    return (<div className="mt-5 listing-page">
+        <div className="listingTitle col-11 ms-auto my-4">Most Visited <span className="listingTitleUnderline">Destinations</span></div>
+        <div className="row d-flex justify-content-center" >
             {Hotel.map(createCard)}
         </div>
-
+        {openModal && <HotelModal />}
     </div>)
 }
 export default Listing
---
+++ src/Components/Listings/hotelModal/hotelModal.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./hotelModal.css"
+const hotelModal = () => {
+    return (
+        <div className="hotelModal">
+            <div>
+                <p>Reservations</p>
+                <p>Request a reservation. We will come back to you shortly.</p>
+                <form>
+                    <div className="d-flex my-4">
+                        <div className="form-group col-6">
+                            <input type="text" className="form-control" id="fName" placeholder="First Name" />
+
+                        </div>
+                        <div className="form-group col-6">
+                            <input type="text" className="form-control" id="lName" placeholder="Last Name" />
+
+                        </div>
+                    </div>
+                    <div className="form-group my-4">
+                        <input type="email" className="form-control" id="email" placeholder="Enter email" />
+                        <small id="emailHelp" className="form-text text-muted">Travel never shares your data with anyone else.</small>
+                    </div>
+                    <div className="d-flex my-4">
+                        <div className="form-group col-6">
+                            <input className="form-control" id="fromDate" placeholder="From" type="text"
+                                onFocus={(e) => (e.target.type = "date")}
+                                onBlur={(e) => (e.target.type = "text")} />
+                        </div>
+                        <div className="form-group col-6">
+                            <input className="form-control" id="toDate" placeholder="To" type="text"
+                                onFocus={(e) => (e.target.type = "date")}
+                                onBlur={(e) => (e.target.type = "text")} />
+                        </div>
+                    </div>
+                    <div className="d-flex my-4">
+                        <div className="form-group col-4">
+                            <input type="number" className="form-control" id="rooms" placeholder="Rooms" />
+                        </div>
+                        <div className="form-group col-4">
+                            <input type="number" className="form-control" id="adults" placeholder="Adults" />
+                        </div>
+                        <div className="form-group col-4">
+                            <input type="number" className="form-control" id="children" placeholder="Children" />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary mx-auto">Book Reservation</button>
+                </form>
+
+            </div>
+        </div>
+    )
+}
+export default hotelModal
